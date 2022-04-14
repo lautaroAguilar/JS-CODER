4d830f917fd80a6b2ea0342ab9fc247f4c8205d4
Lautaro Aguilar <eley.aramis@gmail.com>
2022-04-14 19:05:17 -0300

    me falta terminar de cerrar el ciclo

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-/* /* Ciclo para ingresar número de pedidos
- */
+/* Algoritmo para dar tu presente el cual se habilita con un código (546281) que se revela al final de la clase  */
 
+let entrada = prompt("Ingrese su nombre").toUpperCase();
+let código = 546281
+let error 
+let salida
 
-let pedido = prompt("Ingresar un pedido");
-
-while(pedido != "SALIR" ){
-    alert("El usuario pidió" + " " + pedido + " " + "órdenes");
-    
-    pedido = prompt("Ingresar otro pedido o escribe SALIR si así lo desea");
+while (entrada != "SALIR") {
+    alert("Hola" + " " + entrada + " " + "ingrese el código para obtener el presente");
+    let código = parseInt(prompt("ingrese su código de verificación"));
+    if (código == 546281) {
+        alert("Su asistencia fue pasada correctamente");
+    } else {
+        error = alert("Error, su código no es correcto");
+        salida = prompt("Sí desea salir, sólo escriba esa palabra. Si desea volver a intentarlo, escriba la palabra seguir.").toUpperCase();
+    }
+    switch (salida) {
+        case "SEGUIR":
+            entrada = prompt("Ingrese su nombre nuevamente");
+            break;
+        case "SALIR":
+            alert("Usted decidió salir.");
+            break;
+        default:
+            alert("recargue la página");
+    }
+    entrada = prompt("confirme su decision").toUpperCase();
 }
+
+
+/* do {
+        salida = prompt("Sí desea salir, sólo escriba esa palabra. Si desea volver a intentarlo, escriba la palabra seguir.").toUpperCase();
+    } while (salida != "SALIR") {
+        
+    }
+    entrada = prompt("Ingrese su nombre nuevamente").toUpperCase() */
