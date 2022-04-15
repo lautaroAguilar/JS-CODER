efc10cfc9385022f469a5560503a865b1e26b5e5
Lautaro Aguilar <eley.aramis@gmail.com>
2022-04-15 01:26:36 -0300

    termine el algoritmo de las asistencias

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 /* Algoritmo para dar tu presente el cual se habilita con un código (546281) que se revela al final de la clase  */
 
-let entrada = prompt("Ingrese su nombre").toUpperCase();
-let código = 546281
-let error 
-let salida
+let entrada
+let código = 546281;
+let entradaCódigo
+let error = "No se ingresó un código numérico";
+let salida = "SALIR"
 
-while (entrada != "SALIR") {
+function solicitarNombre() {
+    entrada = prompt("Ingrese su nombre").toUpperCase();
+    alert("Hola" + " " + entrada);
+}
+function solicitarCódigo() {
+    entradaCódigo = parseInt(prompt("Ingresar el código")); 
+    if (entradaCódigo == código) {
+        alert("Su asistencia se pasó correctamente");
+    } else if (entradaCódigo != código) {
+        alert("Su código es incorrecto, intentelo nuevamente.");
+        salida = parseInt(prompt("Vuelva a escribir el código. Si desea salir escriba esa palabra")).toUpperCase(); 
+    } else alert(error);
+}
+
+
+solicitarNombre();
+do {
+    solicitarCódigo();
+} while(salida != "SALIR") {
+    
+}
+
+
+/* while (entrada != "SALIR") {
     alert("Hola" + " " + entrada + " " + "ingrese el código para obtener el presente");
     let código = parseInt(prompt("ingrese su código de verificación"));
     if (código == 546281) {
@@ -22,10 +46,11 @@ while (entrada != "SALIR") {
             alert("Usted decidió salir.");
             break;
         default:
-            alert("recargue la página");
+            ;
+            break;
     }
-    entrada = prompt("confirme su decision").toUpperCase();
-}
+    entrada = prompt("Si decidio seguir, escriba su nombre nuevamente. De lo contrario, escriba salir.").toUpperCase();
+} */
 
 
 /* do {
