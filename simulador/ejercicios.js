077fb0f2f163db42bc8d7be52afd3b94e860325c
Lautaro Aguilar <eley.aramis@gmail.com>
2022-05-10 09:12:50 -0300

    modifique el inicio y cree variables para eliminar el inicio y crear el formulario

--- simulador/ejercicios.js
+++ simulador/ejercicios.js
@@ -131,3 +131,58 @@ pedirCodigo(); */
     
         alert(`Contraseña correcta ${entradaNombre}`)
      */
+
+// CODIGO PARA OBTENER VALOR DE INPUTS Y PONERLO EN UN ARRAY
+/* 
+// obtengo los valores del form para crear objetos con ellos
+
+let contenedor = document.getElementById(`contenedor-form`);
+let formulario = document.getElementById(`formulario`);
+
+
+formulario.addEventListener(`submit`, (e) => {
+    e.preventDefault();
+    //obtenemos los valores de los inputs 
+    const inputs = e.target.querySelectorAll(`input`);
+    
+    class Alumnos {
+        constructor(nombre, apellido, fechaDeNacimiento, nacionalidad) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.nacionalidad = nacionalidad;
+
+        }
+            
+    }
+    //mando al alumno a la planilla
+    let planillaAlumnos = [];
+
+    let alumno = new Alumnos(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value);
+    
+    planillaAlumnos.push(alumno);
+
+    console.log(planillaAlumnos);
+    let contenedor = document.getElementById(`contenedor-form`);
+
+    let h1 = document.createElement("h1");
+    h1.innerText = `Hola ` + alumno.nombre + ` esta es tu ficha`;
+    contenedor.append(h1);
+    let ul = document.createElement(`ul`);
+    
+    let li1 = document.createElement(`li`);
+    li1.innerText = `Nombre:` + alumno.nombre;
+    let li2 = document.createElement(`li`);
+    li2.innerText = `Apellido:` + alumno.apellido;
+    let li3 = document.createElement(`li`);
+    li3.innerText = `Fecha de nacimiento:` + alumno.fechaDeNacimiento;
+    let li4 = document.createElement(`li`);
+    li4.innerText = `Nacionalidad:` + alumno.nacionalidad;
+
+    ul.append(li1, li2, li3, li4);
+    contenedor.append(ul);
+});
+
+
+
+*/
--- simulador/script.js
+++ simulador/script.js
@@ -1,49 +1,52 @@
-// obtengo los calores del form para crear objetos con ellos
+//contenedores
+const container = document.getElementById(`container`);
+const containerText = document.getElementById(`container-text`); 
+const containerButton = document.getElementById(`button-container`);
+//botones
+const button1 = document.getElementById(`button-1`);
+const button2 = document.getElementById(`button-2`);
 
-let contenedor = document.getElementById(`contenedor-form`);
-let formulario = document.getElementById(`formulario`);
+//button1.addEventListener(`click`,);
+//button2.addEventListener(`click`,);
 
-
-formulario.addEventListener(`submit`, (e) => {
-    e.preventDefault();
-    //obtenemos los valores de los inputs 
-    const inputs = e.target.querySelectorAll(`input`);
+// ÁREA DE CREACIÓN DE FUNCIONES
+function eliminarBotoneraYBienvenidos() {
+    containerText.remove();
+    containerButton.remove();
     
-    class Alumnos {
-        constructor(nombre, apellido, fechaDeNacimiento, nacionalidad) {
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.fechaDeNacimiento = fechaDeNacimiento;
-            this.nacionalidad = nacionalidad;
+}
 
-        }
-            
-    }
-    //mando al alumno a la planilla
-    let planillaAlumnos = [];
+function crearFormulario() {
+    //creo el form
+    let form = document.createElement(`form`);
+    container.append(form);
+    form.className = `form-registro`;
+    //creo los inputs y los agrego al form
+    let input1 = document.createElement(`input`);
+    let input2 = document.createElement(`input`);
+    let input3 = document.createElement(`input`);
+    let input4 = document.createElement(`input`);
+    let formRegistro = document.getElementsByClassName(`form-registro`);
+    input1.setAttribute(`placeholder`, "Nombre");
+    input2.setAttribute(`placeholder`, "Apellido");
+    input3.setAttribute(`placeholder`, "Nacionalidad");
+    input4.setAttribute(`placeholder`, "Fecha de nacimiento");  
+    form.append(input1);
+    form.append(input2);
+    form.append(input3);
+    form.append(input4);
+    //creo los botones y los agrego al form
+    let botonReseteo = document.createElement(`a`);
+    let botonRegistro = document.createElement(`a`);
+    botonReseteo.className = `boton`;
+    botonRegistro.className = `boton`;
+    botonReseteo.innerText = `Resetear`;
+    botonRegistro.innerText = `Enviar`;
+    form.append(botonReseteo);
+    form.append(botonRegistro);
 
-    let alumno = new Alumnos(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value);
-    
-    planillaAlumnos.push(alumno);
+}
 
-    console.log(planillaAlumnos);
-    let contenedor = document.getElementById(`contenedor-form`);
-
-    let h1 = document.createElement("h1");
-    h1.innerText = `Hola ` + alumno.nombre + ` esta es tu ficha`;
-    contenedor.append(h1);
-    let ul = document.createElement(`ul`);
-    
-    let li1 = document.createElement(`li`);
-    li1.innerText = `Nombre:` + alumno.nombre;
-    let li2 = document.createElement(`li`);
-    li2.innerText = `Apellido:` + alumno.apellido;
-    let li3 = document.createElement(`li`);
-    li3.innerText = `Fecha de nacimiento:` + alumno.fechaDeNacimiento;
-    let li4 = document.createElement(`li`);
-    li4.innerText = `Nacionalidad:` + alumno.nacionalidad;
-
-    ul.append(li1, li2, li3, li4);
-    contenedor.append(ul);
-});
-
+//eliminarBotoneraYBienvenidos();
+//crearFormulario();
+console.log(container);
